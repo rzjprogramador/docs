f45ffb9c8134ce9b3901ef6d62b2644eafe04b94
rzjprogramador <rzjprogramador@gmail.com>
2022-05-30 11:20:00 -0300

    fix: erros comuns graphql

---
+++ conceitos/04_API/GRAPHQL/conceitos_graphql/erros_comuns_graphql.js
@@ -0,0 +1,8 @@
+
+
+{
+  erro: 'but not in schema',
+  traducao: 'mas não no esquema',
+  causa: 'falta tipar no type defs o schema que esta querendo fazer no resolver',
+  solucao: `tipar no type defs Query{} corretamente a consulta que quer resolver no resolver os dois tem que combinar.`
+}
