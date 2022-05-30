05d84c214aa4b7db287b8f756c70de50e892ca19
rzjprogramador <rzjprogramador@gmail.com>
2022-05-30 09:22:58 -0300

    fix: graphql

---
+++ conceitos/04_API/GRAPHQL/conceitos_graphql/02_typedefs_definicoes_de_tipos.js
@@ -0,0 +1,19 @@
+
+{
+   contexto: `definicao_de_tipos :: typedefs`,
+
+   conceito: `em cada entidade-objeto`,
+
+    importacao: `import { gql } from 'apollo-server'`,
+
+    exportar_string: {
+      exportar: "export const objetoEntidade = gql`..definir..`",
+
+      definir_na_string: {
+        propriedades_da_entidade : `type Entidade {}`,
+        consultas_disponiveis_da_entidade_criada : `type Query {}`,
+      },
+    },
+
+}
+
