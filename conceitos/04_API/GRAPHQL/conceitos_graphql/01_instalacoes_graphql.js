e4e2c20cc055b046668a74c0b661c907c5b48d31
rzjprogramador <rzjprogramador@gmail.com>
2022-05-30 09:18:10 -0300

    fix: graphql

---
+++ conceitos/04_API/GRAPHQL/conceitos_graphql/01_instalacoes_graphql.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+
+const install_graphQl = {
+  npm_inicio: `npm init -y`,
+
+  dependencies: `graphql apollo-server`,
+
+  devDependencies: `@types/graphql`
+
+  servidor_dependencia: `apollo-server`
+
+}
