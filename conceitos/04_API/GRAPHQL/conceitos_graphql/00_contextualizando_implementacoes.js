df149afe0abb3b3526a84af1d7cc438a488edd4f
rzjprogramador <rzjprogramador@gmail.com>
2022-05-31 15:20:57 -0300

    fix: contextualizando

---
+++ conceitos/04_API/GRAPHQL/conceitos_graphql/00_contextualizando_implementacoes.js
@@ -0,0 +1,10 @@
+
+{
+  criacao: `na funcao de criacao nao pedir do client id , nem campos de relacionamento`,
+
+  aplicacao_gerar: [
+    'id', 
+    'id que liga o relacioamento default ex: todos iniciam como USER',
+   ],
+
+}
