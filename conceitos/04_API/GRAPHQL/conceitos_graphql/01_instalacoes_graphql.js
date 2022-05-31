61a3d4727c22c2c445d02e33945dc43e159d9ead
rzjprogramador <rzjprogramador@gmail.com>
2022-05-31 11:27:22 -0300

    fix: graphql tools merge de schemas

--- conceitos/04_API/GRAPHQL/conceitos_graphql/01_instalacoes_graphql.js
+++ conceitos/04_API/GRAPHQL/conceitos_graphql/01_instalacoes_graphql.js
@@ -10,3 +10,29 @@ const install_graphQl = {
   servidor_dependencia: `apollo-server`
 
 }
+
+{
+  contexto: `Mergiar todos schemas graphql`
+  ferramenta: `graphql_tools_merge_schemas `
+  instalacoes: `yarn add -D @graphql-tools/load @graphql-tools/graphql-file-loader`,
+  doc: `https://www.graphql-tools.com/docs/schema-loading`,
+  tutorial: `https://www.youtube.com/watch?v=I79_b7K0rIk`,
+  exemplo: `
+  // no CENTRALIZADOR QUE VAI ENTREGAR OS TYPEDEFS APRA O SERVIDOR APOLLO
+  import { loadSchemaSync } from '@graphql-tools/load'
+  import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
+
+
+  export const typeDefs = loadSchemaSync("./**/*.graphql", {
+    loaders: [new GraphQLFileLoader()]
+  })
+
+  // IMPORTANTE ESTA CHAMADA DE ONDE ESTAO OS ARQUIVOS .GRAPHQL ESTA ASSIM : ./DESTE LUGAR/**SOBE 1 PASTA/ A PROXIMA TODOS QUE TIVER A EXTENSAO *.graphql
+  PASSA UM OBJ DE CONFIGURACAO APRA FAZER LOADERCOM A O OBJ DA CLASSE GraphQLFileLoader()
+
+  // EM CADA ARQUIVO PODE EXPORTAR SUAS DEPENDENCIAS COM #import Entidade from 'caminho/arquivo.graphql'
+
+  // IMPORTANTE QUE CADA PASTA DO MODULO ESTEJA COM O NOME DO ARQUIVO  ACAO.GRAPHQL 
+  
+  `
+}
