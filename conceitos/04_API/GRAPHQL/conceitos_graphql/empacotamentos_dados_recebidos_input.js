7f7d605493ebc83eacd887d17ccdaac1f4fc3f0b
rzjprogramador <rzjprogramador@gmail.com>
2022-06-02 10:15:08 -0300

    ok

--- conceitos/04_API/GRAPHQL/conceitos_graphql/empacotamentos_dados_recebidos_input.js
+++ conceitos/04_API/GRAPHQL/conceitos_graphql/empacotamentos_dados_recebidos_input.js
@@ -4,6 +4,6 @@ empacotamento_dados_recebidos = {
   casoDeUso: `usa-se em Mutation para encapsular dados que serao recebidos`,
   no_typeDefs: `usar a clausula input NomeAcaoInput { campos e tipos a serem usados }`,
   no_typeDefs_em_mutation: `usar a clausula input NomeAcaoInput { campos e tipos a serem usados }`,
-  no_resolver: `no 2º argumento arg ao inves do arg extrair o input usado na mutation ex: { data }`,
+  no_resolver: `no 2º argumento args dentro dele terá o input criado e usado na mutacao, entao dentro de args extrair o input usado na mutacao ex: { data }`,
   no_client: `desempacotar o input aonde enviara os campos variaveis dos dados para o server.`,
 }
