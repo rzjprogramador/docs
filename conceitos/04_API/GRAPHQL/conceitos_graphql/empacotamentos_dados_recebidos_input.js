31d81f68c3312b48b26e2c6524edd2430feae40b
rzjprogramador <rzjprogramador@gmail.com>
2022-06-02 09:46:23 -0300

    fix: input DTO graphql

---
+++ conceitos/04_API/GRAPHQL/conceitos_graphql/empacotamentos_dados_recebidos_input.js
@@ -0,0 +1,9 @@
+empacotamento_dados_recebidos = {
+  contexto: `input`,
+  conceito: `empacotamento dados recebidos com input é como um DTO`,
+  casoDeUso: `usa-se em Mutation para encapsular dados que serao recebidos`,
+  no_typeDefs: `usar a clausula input NomeAcaoInput { campos e tipos a serem usados }`,
+  no_typeDefs_em_mutation: `usar a clausula input NomeAcaoInput { campos e tipos a serem usados }`,
+  no_resolver: `no 2º argumento arg ao inves do arg extrair o input usado na mutation ex: { data }`,
+  no_client: `desempacotar o input aonde enviara os campos variaveis dos dados para o server.`,
+}
