728875688666505d0bd8ae30fc82e0ec8fe3f821
rzjprogramador <rzjprogramador@gmail.com>
2022-06-04 14:42:12 -0300

    ok

---
+++ conceitos/TYPESCRIPT/build_typescript.js
@@ -0,0 +1,9 @@
+{
+  tecnologia: `typescript`,
+  contexto: `build`
+  processo: `
+  script : "build": "tsc --build" `,
+  detalhes: ` // nao informa o arquivo só dentro do script a flag tsc --build`,
+  tutorial: `https://docs.microsoft.com/pt-br/visualstudio/javascript/compile-typescript-code-npm?view=vs-2022`,
+
+}
