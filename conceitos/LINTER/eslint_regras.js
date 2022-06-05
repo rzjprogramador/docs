89c0f3808251462b85222361bf36deb8d60e1982
rzjprogramador <rzjprogramador@gmail.com>
2022-06-04 21:39:45 -0300

    fix: [linter]: regras prettier e eslint

--- conceitos/LINTER/eslint_regras.js
+++ conceitos/LINTER/eslint_regras.js
@@ -2,5 +2,14 @@
   doc: `https://eslint.org/`,
 
   espaco_antes_parenteses_de_funcao: `"space-before-function-paren": ["war", "always"],`,
+  virgula_ultimo_item_obj: `"comma-dangle": ["warn","always" ],`
+  so_usar_constantes: `"prefer-const": "off",`,
+  construtor_nao_util: `"no-useless-constructor": "off",`,
+  sem_uso_antes_de_definir: `"no-use-before-define": "off",`,
+  permitir_funcao_vazia: `"@typescript-eslint/no-empty-function": "off",`,
+  permitir_forcar_conversao_em_boleano: `"@typescript-eslint/strict-boolean-expressions": "off",`,
+  forca_camelCase: `"camelcase": "off",`,
+  permite_usar_template_literal: `"quotes": "off",`,
+
 }
 
