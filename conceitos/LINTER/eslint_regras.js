3e24e822e3da2a5d3693a95d379f70b7573cf7d7
rzjprogramador <rzjprogramador@gmail.com>
2022-06-04 21:19:25 -0300

    fix: [linter]: regras prettier e eslint

---
+++ conceitos/LINTER/eslint_regras.js
@@ -0,0 +1,6 @@
+{
+  doc: `https://eslint.org/`,
+
+  espaco_antes_parenteses_de_funcao: `"space-before-function-paren": ["war", "always"],`,
+}
+
---
+++ conceitos/LINTER/prettier_formatCodigo.js
@@ -0,0 +1,13 @@
+{
+  doc: `https://prettier.io/docs/en/options.html`,
+
+  ponto_e_virgula: `"semi": false`,
+  virgula_a_direita: `"trailingComma": "all"`,
+  aspas_duplas: `"singleQuote": true`,
+  largura_TelaEdicao: `"printWidth": 80`,
+  tab_tamanho: `"tabWidth": 2`,
+  final_de_linha_sistema: `"endOfLine": "lf"`,
+  espaco_dentro_das_chaves: `"bracketSpacing:": true`,
+  quebrar_linha: `"proseWrap:": "always",`,
+}
+
