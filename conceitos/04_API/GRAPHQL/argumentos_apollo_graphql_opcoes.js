d17dac83e8785c34b4969dd6efe545366e609402
rzjprogramador <rzjprogramador@gmail.com>
2022-06-08 10:54:06 -0300

    obj paramtro resolvers

---
+++ conceitos/04_API/GRAPHQL/argumentos_apollo_graphql_opcoes.js
@@ -0,0 +1,46 @@
+{
+  ordem: 1,
+  nome: 'parent | obj',
+  no_objeto_de_config_espera: ``,
+  escopo_uso: ``,
+  acesso: ``,
+  funcionalidade: ``,
+  casos_de_uso: [
+    '',
+    '',
+    ],
+  uso: ``,
+}
+
+//
+
+{
+  ordem: 2,
+  nome: 'args',
+  no_objeto_de_config_espera: ``,
+  escopo_uso: ``,
+  acesso: ``,
+  funcionalidade: ``,
+  casos_de_uso: [
+    '',
+    '',
+    ],
+  uso: ``,
+}
+
+//
+
+{
+  ordem: 3,
+  nome: 'contexto',
+  no_objeto_de_config_espera: `CONTEXT :: METODO QUE RETORNA OBJETOS () => {...}`,
+  escopo_uso: `adicione contexto só no escopo do centralizador de contexto`,
+  acesso: `global via 3º parametro em todos os resolvers`,
+  funcionalidade: `Compartilhar via 3º parametro entre todos resolvers objeto de informacoes e acessoss. `,
+  casos_de_uso: [
+    'o que precisa ser global dentro deste conxto assunto categoria',
+    'api externas',
+    ],
+    uso: ``,
+}
+
