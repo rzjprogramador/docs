eadf7f13f8d537fa44d44e026d1b9fa994ba5cd4
rzjprogramador <rzjprogramador@gmail.com>
2022-06-12 16:40:22 -0300

    fix: ler erro graphql

---
+++ conceitos/00___/erros_graphql.js
@@ -0,0 +1,10 @@
+[ERROR] 16:31:08 
+GraphQLError: Syntax Error: Expected Name, found ")".
+
+onde esta : no graphql/typeDef ::  em type Query { findAllClientePj(): // uma consulta ja é uma funcao nao precisa dos () parenteses só referenciar a funcao igual feito no objeto Query: {}, 
+// solucao: retirar os parenteses
+ler_erro: `no [ERROR] 16:31:08  // aponta o errro, no  GraphQLError: diz o nome do erro ex: Syntax Error // e o que era esperado, e depois de "virgula" o que foi encontrado ai está o que é pra retirar ou consertar no 
+- caso esta falando :: Erro de sintaxe: Nome esperado, encontrado ")".
+// quer dizer estava esperando só um Nome que a referencia de funcao esperava como Retorno e encontrou parenteses ")"
+
+`
