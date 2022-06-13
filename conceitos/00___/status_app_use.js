f5f2b70f284f16cce5b9fc10f6f444e354c03318
rzjprogramador <rzjprogramador@gmail.com>
2022-06-12 22:53:31 -0300

    ok

---
+++ conceitos/00___/status_app_use.js
@@ -0,0 +1,40 @@
+// ==================================
+
+{
+    nivel: 'nivel uso_cliente',
+    ok: [
+        ' [cliente_pj]: Se o ciente errar nao esta cracheando o app da aplicacao',
+    ],
+    todo: [
+        ' [cliente_pj]: ...',
+    ]
+},
+
+// ==================================
+
+{
+    nivel: 'nivel core da app',
+    ok: [
+        ' [cliente_pj]: tratando erros a nivel do core da app ..falta a nivel graphql',
+    ],
+    todo: [
+        ' [cliente_pj]: ...',
+    ]
+},
+
+// ==================================
+
+{
+    nivel: 'nivel graphql',
+    ok: [
+        ' [cliente_pj]: ...',
+    ],
+    todo: [
+        ' [cliente_pj]: falta tratando erros a nivel graphql',
+    ]
+},
+
+// ==================================
+
+
+
