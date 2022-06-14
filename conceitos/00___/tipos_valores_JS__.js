d69a1130f99d762e5d11787628d00b74b23a83d1
rzjprogramador <rzjprogramador@gmail.com>
2022-06-14 14:30:28 -0300

    fix: [javascript_typescript]: valores null e undefined

---
+++ conceitos/00___/tipos_valores_JS__.js
@@ -0,0 +1,37 @@
+// MODELO
+// {
+//   tecnologia: `Javascript & Typescript`,
+//   contexto: `Tipos possiveis de dados de valores de variaveis Js`,
+//   valor_do_tipo: ``,
+//   traducao: ``,
+//   conceito: `` | '' ,
+//   uso: ``,
+//   possiveis_falhas: ``,
+//   solucoes: `` ,
+// }
+//
+
+{
+  tecnologia: `Javascript & Typescript`,
+  contexto: `Tipos possiveis de dados de valores de variaveis Js`,
+  valor_do_tipo: `undefined`,
+  traducao: `indefinido`,
+  conceito: `variavel existe mas nao foi definido um valor` | `existe mas sem valor` | `se vier de uma funcao é que nao tem retorno`,
+  uso: `nao se usa - procura solucao.`,
+  possiveis_falhas: `é possivel definir uma var sem valor só se for let , porque const da erro de sintaxe sem inicializacao da var :: SyntaxError: Missing initializer in const declaration`,
+  solucoes: `Se for variavel Definir um valor na sua declaracao` | `se for funcao dar retorno ` ,
+}
+//
+
+{
+  tecnologia: `Javascript & Typescript`,
+  contexto: `Tipos possiveis de dados de valores de variaveis Js`,
+  valor_do_tipo: `null`,
+  traducao: `nulo`,
+  conceito: `esta iniciando vazio` | 'nulo' ,
+  uso: `definir uma var como null para a pessoa pode dar valor depois`,
+  possiveis_falhas: `...`,
+  solucoes: `...` ,
+}
+//
+
