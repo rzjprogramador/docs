e28f761ad02484812c2f48309e919e09adbf4f25
rzjprogramador <rzjprogramador@gmail.com>
2022-06-15 14:37:29 -0300

    fix: resolvendo path aliases para vitest

---
+++ conceitos/00___/path_aliases_resolvidos.js
@@ -0,0 +1,16 @@
+/*
+> RESOLVENDO PATH ALIASES PARA VITEST
+ lib: yarn add vite-tsconfig-paths
+ doc_lib: https://www.npmjs.com/package/vite-tsconfig-paths
+
+ uso: `
+ no arquivo de configuracao do vitest :: vite.config.js
+ importar e colocar a funcao da lib no array de plugins ex:
+ 
+ import tsconfigPaths from 'vite-tsconfig-paths'
+
+ plugins: [tsconfigPaths()],
+
+
+ `,
+*/
