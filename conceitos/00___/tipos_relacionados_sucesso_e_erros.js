3162b5b05a6dc004bd41d8dce64efd2b80da797f
rzjprogramador <rzjprogramador@gmail.com>
2022-06-16 22:21:22 -0300

    ok

---
+++ conceitos/00___/tipos_relacionados_sucesso_e_erros.js
@@ -0,0 +1,6 @@
+{
+  relacionados: ['dados/data', 'parametros/argumentos', 'onde manda ou salva'],
+  conceito: `Se o erro for em data.`,
+  causa: `Esses dados sao relacionados um depende do outro `,
+  causa: `Procure onde esta mandando ou salvando esses dados passados por param ou recebidos por args`,
+}
