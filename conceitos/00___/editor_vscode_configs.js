5252ae9b84cdb7ebd173edf5bfe6b86da99d84a5
rzjprogramador <rzjprogramador@gmail.com>
2022-06-18 04:28:17 -0300

    color tema vscode

---
+++ conceitos/00___/editor_vscode_configs.js
@@ -0,0 +1,37 @@
+{
+  color_tema: `
+  
+  "workbench.colorCustomizations": {
+
+    // Somente BGs				
+"editor.background":	"#191a2e",			
+"terminal.background":	"#191a2e",	/* Terminal Fundo */		
+"panel.background":	"#191a2e",	/* Painel Baixo terminais - Fundo */		
+"sideBar.background":	"#191a2e",	/* Sidebar Fundo */		
+"sideBarSectionHeader.background":	"#191a2e",	/* header da Sessao no sidebar */
+"tab.activeBackground": "#28152b" /* tabs ativa */		
+"tab.activeBackground":	"#191a2e",	/* Tabs ativa */		
+"tab.inactiveBackground":	"#191a2e",	/* Tabs inativa */		
+"editorGroupHeader.tabsBackground":	"#191a2e",	/* Cor de fundo do container Tabs */		
+				
+// BARRA DE STATUS -- RODAPE				
+"statusBar.background":	"#191a2e",	/* Barra de Status - fundo */		
+"statusBarItem.hoverBackground":	"#191a2e",	/* Items da barra de Status */		
+				
+"activityBar.background": 	"#191a2e",			
+"badge.background":	"#191a2e",			
+"menu.background": 	"#191a2e",			
+"breadcrumb.background": 	"#191a2e",			
+"input.background": 	"#191a2e",	/* Input sidebar */		
+"editorHoverWidget.background": 	"#191a2e",	/* hover Widget sidebar - onde esta o input */		
+				
+// BARRA DE ATIVIDADE - LATERAL SIDEBAR				
+// Color Destaque-Semi				
+"activityBar.activeBackground":	"#2a0042",	/* Barra de Atividade ATIVA- Fundo A */		
+"tab.hoverBackground": 	"#2a0042",	 /* hover tab superior paginas */		
+"list.hoverBackground": 	"#2a0042" 	/* hover list sidebar */		
+  },
+
+
+  `,
+}
