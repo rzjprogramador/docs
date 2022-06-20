046fd739fbafe861581ab036adab5aa8da908f41
rzjprogramador <rzjprogramador@gmail.com>
2022-06-20 11:43:39 -0300

    ok

--- conceitos/00___/links_externos_.js
+++ conceitos/00___/links_externos_.js
@@ -1,19 +1,24 @@
-link_repo: {
+{
   projeto: `Br Dev`,
   link: `https://github.com/codigofalado/brdev`,
 }
 
-link_repo: {
+{
   projeto: `tests E2E - rocket`,
   link: `https://github.com/rocketseat-content/node-tests-e2e`,
 }
 
-link_repo: {
+{
   projeto: `Rest e GraphQL juntos - rocket`,
   link: `https://github.com/rocketseat-content/youtube_graphql_express`,
 }
 
-link_repo: {
+{
   projeto: `test Prisma Super test - rocket- dani`,
   link: `https://github.com/rocketseat-content/youtube-node-testes`,
+} 
+
+{
+  projeto: `Apollo Server Graphql Oficial Github Noticias new CHANGELOG Mudancas aos Logados `,
+  link: `https://github.com/apollographql/apollo-server/blob/main/CHANGELOG.md`,
 }
