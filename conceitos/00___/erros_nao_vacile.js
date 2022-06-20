7b9bcd54b7bb5ab74a2b7938c680dfb1f48361fe
rzjprogramador <rzjprogramador@gmail.com>
2022-06-19 22:35:43 -0300

    ok

---
+++ conceitos/00___/erros_nao_vacile.js
@@ -0,0 +1,3 @@
+{
+  falta_await: `Se vai usar dentro de uma funcao uma outra funcao que seja async sempre coloqeu await antes deste uso... exemplo : const user = await userService.handler(userData) // Prrdi tempo Não passando TEST só por falta de await nest uso de uma funcao importada que era async. #atencao, #naoVacile`;
+}
