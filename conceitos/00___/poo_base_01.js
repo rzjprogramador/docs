07cb8ffc3ba5920ef0d10ce9fc4f6b25180c9685
rzjprogramador <rzjprogramador@gmail.com>
2022-06-21 01:45:22 -0300

    poo

---
+++ conceitos/00___/poo_base_01.js
@@ -0,0 +1,9 @@
+{
+  construtor: {
+    pense: `Sempre pense no constructor() 
+    "O que as outras entidades que serao criadas com este construtor vao ter..entao coloque aqui" .
+    `;
+
+    conceito: `Construtor é a funcao que recebe parametros e vamos usar na instaciacao /invocacao com new para criar uma nova entidade do modelo desta classe um objeto.`;
+  }
+}
