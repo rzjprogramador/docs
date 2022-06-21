4b305482959732c88f716a297d4da35e8591598c
rzjprogramador <rzjprogramador@gmail.com>
2022-06-21 12:19:33 -0300

    ok

---
+++ conceitos/00___/01_aplicacao_inicio_requisitos.js
@@ -0,0 +1,28 @@
+/*
+# DESAFIO
+
+## NARRATIVA: SOLICITACAO:
+
+- o Aluno envia a Submissao de um Desafio
+  ... e a "aplicacao sistema" gera a Correcao e envia o Resultado.
+
+> // EXISTE ENTIDADES NA NARRATIVA E O QUE CONECTA AS ENTIDADES SAO ALGUNS VERBOS. exemplo:
+
+ABSTRACOES: 
+## PARA ABSTRAIR TODAS ENTIDADES : ELAS ESTÃO DEPOIS D INDICATIVOS E TAMBEM ANTES DE DE VERBOS ACOES
+## ACAO_EVENTO: É UM EVENTO E TEM QUE TER ASSOCIADOS PARA ACONTECER
+
+- DEPOIS_DE_INDICATIVOS: (O, A, ELE, UM exemplo: O Aluno ..Aluno é Entidade)
+- ANTES_DE_VERBOS TAMBEM: (ex: ENVIA()..., CORRIGE()...// É UM EVENTO E TEM QUE TER ASSOCIADOS PARA ACONTECER, GERA()...
+### ENTIDADES_EXTRAIDAS:
+
+> Aluno
+> Submissao
+> Desafio
+> Correcao
+> Resultado
+
+//
+
+
+*/
