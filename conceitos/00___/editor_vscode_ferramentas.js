7702990056445bce1551fe82f057b828c02e21d5
rzjprogramador <rzjprogramador@gmail.com>
2022-06-21 02:15:54 -0300

    ok

---
+++ conceitos/00___/editor_vscode_ferramentas.js
@@ -0,0 +1,7 @@
+{
+  replace: `
+  Mudar nome mesmo maiusculos e minnusculos
+  Ctrl + F // marque a 1ª caixa AB que preserva case // pressione a ultima Replace All
+  testar: Modalidade modalidade Cantar cantar
+  `;
+}
---
+++ conceitos/00___/poo_typescript.js
@@ -0,0 +1,8 @@
+{
+{
+  tipo_generico: {
+    pense: `no Pai "vou receber" <T> ..onde vou usar.. "vou passar T"  .."Vou receber o Tipo e vou passar o tipo" .`,
+
+  },
+  
+}
