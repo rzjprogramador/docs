dc179d09ef5fbcc7522fd05b7620fc5f0765fb0b
rzjprogramador <rzjprogramador@gmail.com>
2022-06-22 12:03:42 -0300

    ok

---
+++ conceitos/00___/exercicios/logica_pgm.js
@@ -0,0 +1,15 @@
+/* ---------------------- // ---------------------- */
+/* FUNCAO QUE RECEBE DOIS NUMEROS E RETORNA O MAIOR ENTRE ELES */
+//
+function numeroMaior(num1, num2) {
+  if (num1 > num2) {
+    return `Enviados ${num1} e ${num2} :: O MAIOR É ${num1}`;
+  }
+  return `Enviados ${num1} e ${num2} :: O MAIOR É ${num2}`;
+}
+
+console.log(numeroMaior(1, 200));
+
+/* ---------------------- // ---------------------- */
+/*  */
+//
