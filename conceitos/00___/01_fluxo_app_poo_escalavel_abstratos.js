1576cd58741467e44ef41cffff09a02118184e75
rzjprogramador <rzjprogramador@gmail.com>
2022-06-22 02:58:04 -0300

    ok

---
+++ conceitos/00___/01_fluxo_app_poo_escalavel_abstratos.js
@@ -0,0 +1,11 @@
+// FLUXO APP ESCALAVEL POO ABSTRATOS
+{
+  entidade: {
+    contrato_entidade: `Classe Abstrata que modela entidades`,
+    implementacao_conrtato: `classe conreta que herda a abstrata classe Entidade`,
+    propriedades_de_Pai_para_Filho: `de modo privados só para leitura no construtor`,
+    tipagem_das_propriedades: `type com todas as props que serao recebidas na construcao`
+
+    metodos: `METODOS NAO CRIO NO OBJETO DA ENTIDADE --CRIO NOS REPOSITORIOS O CONTRATO E IMPLEMENTO NO USECASES`,
+  }
+}
