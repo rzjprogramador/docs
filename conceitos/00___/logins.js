61c0618fead91baefba53bcbf164d6d3c5baebd9
rzjprogramador <rzjprogramador@gmail.com>
2022-06-28 11:03:29 -0300

    OK

---
+++ conceitos/00___/logins.js
@@ -0,0 +1,60 @@
+{
+  `
+  GOOGLE
+  rzjprogramador :: PASS: Rzj_PAI_MAE_EU
+  .........
+  
+  DRIVE:
+  GDrive:
+  driverzjaulas         // Rzj3020...
+  driveraulasdois    // Rzj3020...
+  
+  ...
+  CONECTIVIDADE LOGS
+  CLI :
+  mixflex : google :: conectividademixflex@gmail.com // cli102030 fone: rzj_VIVO
+  ...
+  
+  =======================
+  
+  SITES_CLIENTES : 
+  Site Google> Mixflex >> 
+  http://www.sites.google.com/view/mixflex-entregas-sp
+  
+  =======================
+  
+  ...
+  =======================
+  INFORME CLIENTE GOOGLE SITE
+  
+  .........................................................
+  
+  Endereco WebSite no GoogleSites : 
+  www.sites.google.com/view/mixflex-entregas-sp
+  
+  .........................................................
+  # PAINEL ADMINISTRATIVO
+  * dados para manutencao:
+  * 
+  endereco_manutencao_site: 
+  email: conectividademixflex@gmail.com
+  senha: cli102030
+  
+  detalhes: Voce tem que estar logado a esta conta google para poder entrar no painel administrativo e editar o site.
+  .........................................................
+  
+  • ADMINISTRACAO FORMULARIOS
+  • endereco_formularios : https://docs.google.com/forms/u/1/
+  • 
+  • observacao: A edicao ou inclusao de novos formularios vem do google formularios acesse o link para deixa-lo da forma desejada ou criar novos e adiciona-los ao site via adminsitracao do site
+  • .........................................................
+  # Publicacao GoogleSite no Ar
+  O google libera por 72 horas o prototipo do site para poder testar e editar ..antes de confirmar se sera publicado na rede
+  
+  # Ferramenta e solicitacoes
+  A ferramenta é um pouco limitada algumas solicitacoes vou ter que pesquisar e testar pra ver se vai ser possivel, mas mande suas recomendaçoes encima do que ja esta ai no endereco do Site.
+  • .........................................................
+  
+  =======================
+  `;
+}
