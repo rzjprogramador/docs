da865ab931072db8378cb0031bee42ffa820515f
rzjprogramador <rzjprogramador@gmail.com>
2022-06-28 09:05:55 -0300

    ok

--- conceitos/00___/erros_nao_vacile.js
+++ conceitos/00___/erros_nao_vacile.js
@@ -1,3 +1,17 @@
 {
   falta_await: `Se vai usar dentro de uma funcao uma outra funcao que seja async sempre coloqeu await antes deste uso... exemplo : const user = await userService.handler(userData) // Prrdi tempo Não passando TEST só por falta de await nest uso de uma funcao importada que era async. #atencao, #naoVacile`;
+
+  erros_encontrar: `
+  O erro undefined pode ser falta de return em uma funcao que retorna um resultado.
+....
+Conceito Erros -- Forma Encontrar ::; 
+Se o console informa erro anlinha 18  >> Pode ser o inicio do erro na linha antes a 17 ou onde a variavel usada nestas linhas esta com problema na sua inicializacao ...
+......
+undefined / indefinido como retorno de uma funcao pode ser falta de retorno/ return
+TUDO QUE NAO RETORNA E INDEFINIDO
+
+aula : Roger :: https://youtu.be/gqt5gnGTpfY
+....
+`,
+
 }
