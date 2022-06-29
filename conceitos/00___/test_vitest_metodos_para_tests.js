e709572512478d77e31e9a408a5641e2399fe3b4
rzjprogramador <rzjprogramador@gmail.com>
2022-06-29 09:18:15 -0300

    erro asincrono

--- conceitos/00___/test_vitest_metodos_para_tests.js
+++ conceitos/00___/test_vitest_metodos_para_tests.js
@@ -15,5 +15,18 @@
 {
   nao_deve_retornar_vazio: `not.toEqual('')`,
   nao_deve_retornar_nulo: `not.toBeNull()`,
+  funcao_async: `
+  .rejects.toEqual
+
+  CHAMANDO UMA FUNCAO MAIS QUE UMA VEZ DEVE SER REJEITADA  E A REJEICAO IGUAL A ALGO USADO NA SUA ORIGEM com : .rejects.toEqual
+
+  EXEMPLO:
+  // ESTA FUNCAO DE CRIACAO SERA CHAMADA 2 VEZES E TEM QUE SER REJEITADA E A REJEICAO IGUAL A MENSAGEM
+    await createClienteService.execute(clienteData)
+
+    expect(createClienteService.execute(clienteData)).rejects.toEqual(new Error(MSG_EXCEPTION.INCORRECT))
+  })
+  `,
+  
 
 }
