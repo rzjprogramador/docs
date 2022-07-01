7da8dbf9cd438dc1570fd16ebb2fd7e3bb520bdd
rzjprogramador <rzjprogramador@gmail.com>
2022-07-01 11:06:34 -0300

    fix: servidor html

---
+++ conceitos/00___/html_servidor.js
@@ -0,0 +1,5 @@
+{
+  comando: `vá pelo terminal ate a ásta alvo e :: python3 -m http.server `;
+  uso: `vai abrir uma pagina web com este servidor html`;
+  tutorial: `https://www.dev2qa.com/how-to-fix-error-access-to-script-at-from-origin-null-has-been-blocked-by-cors-policy-cross-origin-requests-are-only-supported-for-protocol-schemes-http-data-chrome-chrome-extension-chrome-u/`;
+}
