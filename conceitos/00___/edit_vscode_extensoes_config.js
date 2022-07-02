b23a1d0d5c9f1d86d14a2a14cb5f04c5920c7537
rzjprogramador <rzjprogramador@gmail.com>
2022-07-02 18:19:03 -0300

    ok

---
+++ conceitos/00___/edit_vscode_extensoes_config.js
@@ -0,0 +1,7 @@
+{
+  http_rest_cliente: `
+  // Usar variavel de ambiente do .vscode arquivo
+  aperte F1 >> digite : rest-client // escolha rest-client Switch enviroments escolha a variavel definida no settings.json do projeto
+  
+  `
+}
