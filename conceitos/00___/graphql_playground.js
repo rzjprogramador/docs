9d7359563b12db844a643bf75f874a9377d7797c
rzjprogramador <rzjprogramador@gmail.com>
2022-07-02 22:44:04 -0300

    ok

---
+++ conceitos/00___/graphql_playground.js
@@ -0,0 +1,13 @@
+{
+  limpar_playground_graphqi: `
+  via localStorage
+
+  inspecionar pagina html
+  application/ localStorage/ http://localhost:<PORTA ONDE ESTA SUBINDO O SERVER GQL>
+  lá encima pode clicar me cleanAll
+
+  obs: para ficar poucas consultas a cada consulta apague a mesma que ele vai gravar os nomes de consulta
+
+  
+  `,
+}
