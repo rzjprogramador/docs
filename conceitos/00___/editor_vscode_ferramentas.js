12ce9a075455474184329afaeea100bd912d829b
rzjprogramador <rzjprogramador@gmail.com>
2022-07-04 00:50:50 -0300

    vscode

--- conceitos/00___/editor_vscode_ferramentas.js
+++ conceitos/00___/editor_vscode_ferramentas.js
@@ -1,7 +1,9 @@
 {
   replace: `
-  Mudar nome mesmo maiusculos e minnusculos
+  Mudar nome mesmo maiusculos e minusculos
   Ctrl + F // marque a 1ª caixa AB que preserva case // pressione a ultima Replace All
   testar: Modalidade modalidade Cantar cantar
   `;
+
+  mostrar_barra_menu_bar_do_topo: 'tecla Alt -- ou  view/appearence/show menu bar',
 }
