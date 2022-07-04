09560c43f9451d5af354f022c3624e550a979b78
rzjprogramador <rzjprogramador@gmail.com>
2022-07-04 00:53:58 -0300

    vscode

--- conceitos/00___/editor_vscode_backp_settings.js
+++ conceitos/00___/editor_vscode_backp_settings.js
@@ -1,304 +1,242 @@
 {
-  backup_settings_vscode: `
-  
-  {
-    // RZJ
-    "workbench.colorCustomizations": {
-      "tab.activeBackground": "#28152b" /* tabs superior ativa */
+  // RZJ
+  "workbench.colorCustomizations": {
+    "tab.activeBackground": "#250069" /* tabs superior ativa */
+  },
+
+  "files.autoSave": "afterDelay",
+  "editor.lineHeight": 24,
+  "update.mode": "none",
+  "zenMode.silentNotifications": true,
+  "editor.wordWrap": "on",
+  "editor.matchBrackets": "never",
+  "workbench.panel.defaultLocation": "bottom",
+  "git.autofetch": true,
+  "editor.defaultFormatter": "esbenp.prettier-vscode", // EDITOR PADRAO VSCODE
+
+  "[prisma]": {
+    "editor.defaultFormatter": "Prisma.prisma",
+    "editor.formatOnSave": true
+  },
+  "todo-tree.highlights.customHighlight": {
+    "TODO": {
+      "background": "191622"
     },
-  
-    "files.autoSave": "afterDelay",
-    "editor.lineHeight": 24,
-    "update.mode": "none",
-    "zenMode.silentNotifications": true,
-    "editor.wordWrap": "on",
-    "editor.matchBrackets": "never",
-    "workbench.panel.defaultLocation": "bottom",
-    "git.autofetch": true,
-    "editor.defaultFormatter": "esbenp.prettier-vscode", // EDITOR PADRAO VSCODE
-  
-    "[prisma]": {
-      "editor.defaultFormatter": "Prisma.prisma",
-      "editor.formatOnSave": true
+    "TODO_CONCEITO": {
+      "background": "191622"
     },
-    "todo-tree.highlights.customHighlight": {
-      "TODO": {
-        "background": "191622"
-      },
-      "TODO_CONCEITO": {
-        "background": "191622"
-      },
-      "TODO_PGM": {
-        "background": "191622"
-      },
-      "TODO_TEST": {
-        "background": "191622"
-      },
-      "TODO_PROJETINHO": {
-        "background": "191622"
-      },
-      "BUG": {
-        "icon": "bug",
-        "background": "IndianRed"
-      },
-      "HACK": {
-        "icon": "tools"
-      },
-      "FIXME": {
-        "icon": "flame",
-        "background": "IndianRed"
-      },
-      "XXX": {
-        "icon": "$(close)"
+    "TODO_PGM": {
+      "background": "191622"
+    },
+    "TODO_TEST": {
+      "background": "191622"
+    },
+    "TODO_PROJETINHO": {
+      "background": "191622"
+    },
+    "BUG": {
+      "icon": "bug",
+      "background": "IndianRed"
+    },
+    "HACK": {
+      "icon": "tools"
+    },
+    "FIXME": {
+      "icon": "flame",
+      "background": "IndianRed"
+    },
+    "XXX": {
+      "icon": "$(close)"
+    }
+  },
+
+  // INTELISENSE RZJ
+  "editor.acceptSuggestionOnCommitCharacter": false,
+  "editor.suggestOnTriggerCharacters": true,
+  "editor.tabCompletion": "on",
+  "editor.wordBasedSuggestions": true,
+  "editor.snippetSuggestions": "inline",
+  // Emmet
+  "emmet.syntaxProfiles": {
+    "javascript": "jsx"
+  },
+  "emmet.includeLanguages": {
+    "django-html": "html",
+    "javascript": "javascriptreact",
+    "typescript": "typescriptreact"
+  },
+  "emmet.triggerExpansionOnTab": false,
+  "emmet.showExpandedAbbreviation": "never",
+
+  // ---
+
+  // Editor and breadcrumbs // INTELISENSE
+  "breadcrumbs.enabled": false,
+  "editor.renderControlCharacters": false,
+  "editor.tabSize": 2,
+  "editor.renderLineHighlight": "gutter",
+  "editor.rulers": [79, 120],
+  "editor.minimap.enabled": false,
+  "editor.fontSize": 16,
+  "editor.detectIndentation": true,
+  "editor.formatOnPaste": false,
+  "editor.cursorSmoothCaretAnimation": true,
+  "editor.mouseWheelScrollSensitivity": 2,
+  "editor.smoothScrolling": true,
+  "editor.mouseWheelZoom": true,
+  "editor.linkedEditing": true,
+  "editor.fontFamily": "'Operator Mono Lig Book', 'SF Mono', Consolas, 'Source Code Pro', 'Dank Mono', 'Fira Code', Menlo, 'Inconsolata', 'Droid Sans Mono', 'DejaVu Sans Mono', 'Ubuntu Mono', 'Courier New', Courier, Monaco, monospace",
+  "editor.glyphMargin": false,
+  "editor.fontLigatures": false,
+  "editor.formatOnSave": true,
+  "editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true,
+    "source.fixAll": true
+  },
+  "javascript.format.insertSpaceAfterConstructor": true,
+  "typescript.format.insertSpaceAfterConstructor": true,
+  "typescript.format.insertSpaceBeforeFunctionParenthesis": true,
+  "workbench.tree.indent": 15,
+  "git.confirmSync": false,
+
+  // Workbench
+  "workbench.startupEditor": "newUntitledFile",
+  "workbench.editor.labelFormat": "short",
+  "workbench.iconTheme": "material-icon-theme",
+  "workbench.editor.enablePreview": false,
+  "workbench.sideBar.location": "right",
+  "workbench.list.smoothScrolling": true,
+  "workbench.colorTheme": "OM Theme (Night Owl Italic)",
+  "workbench.editorAssociations": {
+    "*.ipynb": "jupyter.notebook.ipynb"
+  },
+
+  // Explorer
+  "explorer.compactFolders": false,
+  "explorer.confirmDelete": true,
+
+  // Git
+  "git.enableSmartCommit": true,
+  "diffEditor.ignoreTrimWhitespace": true,
+
+  // Debug
+  "debug.console.fontSize": 18,
+
+  // Files and search
+  "files.defaultLanguage": "{activeEditorLanguage}",
+  "files.exclude": {
+    "USE_GITIGNORE": true
+  },
+  "search.exclude": {
+    "**/node_modules": true,
+    "**/bower_components": true,
+    "**/coverage": true,
+    "**/dist": true,
+    "**/build": true,
+    "**/.build": true,
+    "**/.gh-pages": true
+  },
+
+  // Terminal
+  "terminal.integrated.copyOnSelection": false,
+  "terminal.integrated.cursorBlinking": true,
+  "terminal.integrated.cursorStyle": "line",
+  // "terminal.integrated.shell.linux": "zsh",
+  "terminal.external.linuxExec": "x-terminal-emulator",
+  "terminal.integrated.fontFamily": "'Operator Mono Lig Book', 'Fira Code', 'SF Mono', Consolas, 'Source Code Pro', 'Dank Mono', Menlo, 'Inconsolata', 'Droid Sans Mono', 'DejaVu Sans Mono', 'Ubuntu Mono', 'Courier New', Courier, Monaco, monospace",
+  "terminal.integrated.fontSize": 18,
+  "terminal.integrated.drawBoldTextInBrightColors": false,
+  "terminal.integrated.enableFileLinks": false,
+  "terminal.integrated.env.linux": {
+    // "PORT": "4000"
+  },
+
+  // Javascript & TypeScript
+  "javascript.preferences.quoteStyle": "single",
+  "typescript.preferences.quoteStyle": "single",
+  "javascript.validate.enable": false,
+  "javascript.updateImportsOnFileMove.enabled": "prompt",
+  "typescript.updateImportsOnFileMove.enabled": "prompt",
+  "javascript.suggest.autoImports": true,
+  "typescript.suggest.autoImports": true,
+  "[javascript]": {
+    "editor.suggest.showKeywords": false
+  },
+
+  // Code runner
+  "code-runner.clearPreviousOutput": true,
+  "code-runner.ignoreSelection": true,
+  "code-runner.saveFileBeforeRun": true,
+  "code-runner.runInTerminal": true,
+  "code-runner.preserveFocus": false,
+  "code-runner.executorMap": {
+    "python": "python3 -u",
+    "code-runner.clearPreviousOutput": false,
+    "typescript": "npx ts-node --files --transpile-only -r tsconfig-paths/register",
+    ".js": "clear && node"
+  },
+
+  // Material icon theme extension
+  "material-icon-theme.hidesExplorerArrows": true,
+  "material-icon-theme.folders.theme": "classic",
+  "material-icon-theme.folders.color": "#6273a6",
+
+  // Colorize (in big files you may want to disable this)
+  "colorize.hide_current_line_decorations": false,
+  "colorize.include": [".tsx", ".jsx", ".ts", ".js"],
+  "colorize.languages": [
+    "typescriptreact",
+    "javascriptreact",
+    "javascript",
+    "typescript",
+    "css",
+    "sass",
+    "scss",
+    "less",
+    "pcss",
+    "sss",
+    "stylus",
+    "xml",
+    "svg",
+    "json",
+    "jsonc",
+    "yaml"
+  ],
+  "colorize.colorized_colors": [
+    // "BROWSERS_COLORS", // this is reaaally annoying
+    "HEXA",
+    "RGB",
+    "HSL"
+  ],
+  "colorize.enable_search_variables": false,
+
+  // CSpell checker extension
+  "cSpell.enabled": false,
+  "cSpell.language": "en,pt,pt_BR",
+  "cSpell.enableFiletypes": ["shellscript"],
+
+  // XML Language Support by Red Hat
+  "redhat.telemetry.enabled": true,
+
+  // VsCode Action Buttons
+  "actionButtons": {
+    "defaultColor": "#8be9fd", // Can also use string color names.
+    "loadNpmCommands": false, // Disables automatic generation of actions for npm commands.
+    "reloadButton": "🗘", // Custom reload button text or icon (default ↻). null value enables automatic reload on configuration change
+    "commands": [
+      {
+        "cwd": "./", // Terminal initial folder ${workspaceFolder} and os user home as defaults
+        "name": "Open dictionary",
+        "color": "#8be9fd",
+        "singleInstance": true,
+        "command": "code -r ~/Development/custom-cspell-dictionary.txt; exit" // This is executed in the terminal.
       }
-    },
-  
-    // INTELISENSE RZJ
-    "editor.acceptSuggestionOnCommitCharacter": false,
-    "editor.suggestOnTriggerCharacters": true,
-    "editor.tabCompletion": "on",
-    "editor.wordBasedSuggestions": true,
-    "editor.snippetSuggestions": "inline",
-    // Emmet
-    "emmet.syntaxProfiles": {
-      "javascript": "jsx"
-    },
-    "emmet.includeLanguages": {
-      "django-html": "html",
-      "javascript": "javascriptreact",
-      "typescript": "typescriptreact"
-    },
-    "emmet.triggerExpansionOnTab": false,
-    "emmet.showExpandedAbbreviation": "never",
-  
-    // ---
-  
-    // Editor and breadcrumbs // INTELISENSE
-    "breadcrumbs.enabled": false,
-    "editor.renderControlCharacters": false,
-    "editor.tabSize": 2,
-    "editor.renderLineHighlight": "gutter",
-    "editor.rulers": [79, 120],
-    "editor.minimap.enabled": false,
-    "editor.fontSize": 16,
-    "editor.detectIndentation": true,
-    "editor.formatOnPaste": false,
-    "editor.cursorSmoothCaretAnimation": true,
-    "editor.mouseWheelScrollSensitivity": 2,
-    "editor.smoothScrolling": true,
-    "editor.mouseWheelZoom": true,
-    "editor.linkedEditing": true,
-    "editor.fontFamily": "'Operator Mono Lig Book', 'SF Mono', Consolas, 'Source Code Pro', 'Dank Mono', 'Fira Code', Menlo, 'Inconsolata', 'Droid Sans Mono', 'DejaVu Sans Mono', 'Ubuntu Mono', 'Courier New', Courier, Monaco, monospace",
-    "editor.glyphMargin": false,
-    "editor.fontLigatures": false,
-    "editor.formatOnSave": true,
-    "editor.codeActionsOnSave": {
-      "source.fixAll.eslint": true,
-      "source.fixAll": true
-    },
-  
-    "workbench.statusBar.visible": false,
-    "javascript.format.insertSpaceAfterConstructor": true,
-    "typescript.format.insertSpaceAfterConstructor": true,
-    "typescript.format.insertSpaceBeforeFunctionParenthesis": true,
-    "workbench.tree.indent": 15,
-    "git.confirmSync": false,
-  
-    // Workbench
-    "workbench.startupEditor": "newUntitledFile",
-    "workbench.editor.labelFormat": "short",
-    "workbench.iconTheme": "material-icon-theme",
-    "workbench.editor.enablePreview": false,
-    "workbench.sideBar.location": "right",
-    "window.menuBarVisibility": "toggle",
-    "workbench.list.smoothScrolling": true,
-    "workbench.colorTheme": "OM Theme (Night Owl Italic)",
-    "workbench.editorAssociations": {
-      "*.ipynb": "jupyter.notebook.ipynb"
-    },
-  
-    // Explorer
-    "explorer.compactFolders": false,
-    "explorer.confirmDelete": true,
-  
-    // Git
-    "git.enableSmartCommit": true,
-    "diffEditor.ignoreTrimWhitespace": true,
-  
-    // Debug
-    "debug.console.fontSize": 18,
-  
-    // Files and search
-    "files.defaultLanguage": "{activeEditorLanguage}",
-    "files.exclude": {
-      "USE_GITIGNORE": true
-    },
-    "search.exclude": {
-      "**/node_modules": true,
-      "**/bower_components": true,
-      "**/coverage": true,
-      "**/dist": true,
-      "**/build": true,
-      "**/.build": true,
-      "**/.gh-pages": true
-    },
-  
-    // Terminal
-    "terminal.integrated.copyOnSelection": false,
-    "terminal.integrated.cursorBlinking": true,
-    "terminal.integrated.cursorStyle": "line",
-    // "terminal.integrated.shell.linux": "zsh",
-    "terminal.external.linuxExec": "x-terminal-emulator",
-    "terminal.integrated.fontFamily": "'Operator Mono Lig Book', 'Fira Code', 'SF Mono', Consolas, 'Source Code Pro', 'Dank Mono', Menlo, 'Inconsolata', 'Droid Sans Mono', 'DejaVu Sans Mono', 'Ubuntu Mono', 'Courier New', Courier, Monaco, monospace",
-    "terminal.integrated.fontSize": 18,
-    "terminal.integrated.drawBoldTextInBrightColors": false,
-    "terminal.integrated.enableFileLinks": false,
-    "terminal.integrated.env.linux": {
-      // "PORT": "4000"
-    },
-  
-    // Javascript & TypeScript
-    "javascript.preferences.quoteStyle": "single",
-    "typescript.preferences.quoteStyle": "single",
-    "javascript.validate.enable": false,
-    "javascript.updateImportsOnFileMove.enabled": "prompt",
-    "typescript.updateImportsOnFileMove.enabled": "prompt",
-    "javascript.suggest.autoImports": true,
-    "typescript.suggest.autoImports": true,
-    "[javascript]": {
-      "editor.suggest.showKeywords": false
-    },
-  
-    // Python
-    "[python]": {
-      "editor.defaultFormatter": "ms-python.python",
-      "editor.tabSize": 4,
-      "editor.insertSpaces": true,
-      "editor.formatOnSave": true,
-      "editor.formatOnType": true
-    },
-    "python.formatting.autopep8Args": [
-      "--indent-size=4"
-      // "--ignore=E111",
-    ],
-    "python.linting.flake8Args": [
-      // "--ignore=E111",
-    ],
-    "python.linting.pylintArgs": [
-      "--load-plugins=pylint_django",
-      "--errors-only"
-    ],
-    "python.pythonPath": "/bin/python3.8",
-    "python.linting.flake8Enabled": true,
-    "python.linting.mypyEnabled": true,
-    "python.testing.unittestEnabled": true,
-  
-    // Django
-    "files.associations": {
-      "*.html": "html",
-      "**/templates/*.html": "django-html",
-      "**/templates/*": "django-txt",
-      "**/requirements{/**,*}.{txt,in}": "pip-requirements"
-    },
-  
-    // ##################### Extensions ##################### //
-  
-    // Bracket pair colorizer
-    "bracket-pair-colorizer-2.colors": [
-      "#8BE9FD",
-      "#50FA7B",
-      "#FFB86C",
-      "#FF79C6",
-      "#BD93F9",
-      "#F1FA8C"
-    ],
-    "bracket-pair-colorizer-2.colorMode": "Consecutive",
-    "bracket-pair-colorizer-2.forceUniqueOpeningColor": false,
-    "bracket-pair-colorizer-2.forceIterationColorCycle": false,
-    "bracket-pair-colorizer-2.showBracketsInGutter": true,
-    "bracket-pair-colorizer-2.showBracketsInRuler": true,
-    "bracket-pair-colorizer-2.showVerticalScopeLine": false,
-    "bracket-pair-colorizer-2.showHorizontalScopeLine": false,
-    "bracket-pair-colorizer-2.unmatchedScopeColor": "#FF5555",
-  
-    // Code runner
-    "code-runner.clearPreviousOutput": true,
-    "code-runner.ignoreSelection": true,
-    "code-runner.saveFileBeforeRun": true,
-    "code-runner.runInTerminal": true,
-    "code-runner.preserveFocus": false,
-    "code-runner.executorMap": {
-      "python": "python3 -u",
-      "code-runner.clearPreviousOutput": false,
-      "typescript": "npx ts-node --files --transpile-only -r tsconfig-paths/register",
-      ".js": "clear && node"
-    },
-  
-    // Material icon theme extension
-    "material-icon-theme.hidesExplorerArrows": true,
-    "material-icon-theme.folders.theme": "classic",
-    "material-icon-theme.folders.color": "#6273a6",
-  
-    // Colorize (in big files you may want to disable this)
-    "colorize.hide_current_line_decorations": false,
-    "colorize.include": [".tsx", ".jsx", ".ts", ".js"],
-    "colorize.languages": [
-      "typescriptreact",
-      "javascriptreact",
-      "javascript",
-      "typescript",
-      "css",
-      "sass",
-      "scss",
-      "less",
-      "pcss",
-      "sss",
-      "stylus",
-      "xml",
-      "svg",
-      "json",
-      "jsonc",
-      "yaml"
-    ],
-    "colorize.colorized_colors": [
-      // "BROWSERS_COLORS", // this is reaaally annoying
-      "HEXA",
-      "RGB",
-      "HSL"
-    ],
-    "colorize.enable_search_variables": false,
-  
-    // CSpell checker extension
-    "cSpell.enabled": false,
-    "cSpell.language": "en,pt,pt_BR",
-    "cSpell.enableFiletypes": ["shellscript"],
-  
-    // XML Language Support by Red Hat
-    "redhat.telemetry.enabled": true,
-  
-    // VsCode Action Buttons
-    "actionButtons": {
-      "defaultColor": "#8be9fd", // Can also use string color names.
-      "loadNpmCommands": false, // Disables automatic generation of actions for npm commands.
-      "reloadButton": "🗘", // Custom reload button text or icon (default ↻). null value enables automatic reload on configuration change
-      "commands": [
-        {
-          "cwd": "./", // Terminal initial folder ${workspaceFolder} and os user home as defaults
-          "name": "Open dictionary",
-          "color": "#8be9fd",
-          "singleInstance": true,
-          "command": "code -r ~/Development/custom-cspell-dictionary.txt; exit" // This is executed in the terminal.
-        }
-      ]
-    },
-    "bracket-pair-colorizer-2.depreciation-notice": false,
-    "security.workspace.trust.untrustedFiles": "newWindow",
-    "[dockercompose]": {
-      "editor.defaultFormatter": "esbenp.prettier-vscode"
-    },
-    "git.ignoreRebaseWarning": true
-  }
-  
-  
-  `,
+    ]
+  },
+  "bracket-pair-colorizer-2.depreciation-notice": false,
+  "security.workspace.trust.untrustedFiles": "newWindow",
+  "[dockercompose]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "git.ignoreRebaseWarning": true
 }
